a6fff949d82a817a9c3316d0303019986852ce9c
Madeleinesc <madeleine.s.cosway@gmail.com>
2022-01-31 13:37:58 +1030

    added second part of work section - projects

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar'; //import from Navbar
 import Intro from './components/Intro'; // import from Intro
 import About from './components/About'; // import from About
 import Work from './components/Work'; // import from Work
+import Projects from './components/Projects'; // import from Projects
 
 //import logo from './logo.svg';
 import './App.css';
@@ -18,6 +19,7 @@ function App() {
       <Intro/>
       <About/>
       <Work/>
+      <Projects/>
     </div>
   );
 }
---
+++ src/components/Projects.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div className="container p-3">
+            <div className="row">
+                <div className="col-md-6">
+                    <a href="https://rhjeffries.github.io/ideal-telegram/" ><img src="./assets/moodvies.png" className="project-img img-fluid" data-aos="fade-up" /></a>
+                    <br />
+                    <br />
+                    <p className="lead" data-aos="fade-up">
+                        Moodvies - 2021
+                    </p>
+                    <a href="https://github.com/RHJeffries/ideal-telegram" className="project-links" data-aos="fade-up"><p>
+                        An app that helps you choose movies based on your current mood. Click on the image to view the live project or
+                        click here for the project repo.
+                    </p></a>
+                    <br />
+                    <br />
+                </div>
+
+                <div className="col-md-6"></div>
+
+                <div className="col-md-6"></div>
+
+                <div className="col-md-6">
+                    <a href="https://the-book-was-better-book-club.herokuapp.com/"><img src="./assets/bookwasbetter.png" className="project-img img-fluid" data-aos="fade-up"/></a>
+                    <br />
+                    <br />
+                    <p className="lead" data-aos="fade-up">
+                        The Book Was Better - 2021
+                    </p>
+                    <a href="https://github.com/madeleinesc/The-Book-Was-Better" className="project-links" data-aos="fade-up"><p>
+                        A book club that reads books based solely on whether they have been turned into movies. Click on the image to
+                        view the live project or click here for the project repo.
+                    </p></a>
+                    <br />
+                    <br />
+                </div>
+
+
+                <div className="col-md-6">
+                    <a href="https://immense-bastion-97694.herokuapp.com/"><img src="./assets/notetaker.png" className="project-img img-fluid" data-aos="fade-up"/></a>
+                    <br />
+                    <br />
+                    <p className="lead" data-aos="fade-up">
+                        Note Taker - 2021
+                    </p>
+                    <a href="https://github.com/madeleinesc/note-taker-app" className="project-links" data-aos="fade-up"><p>
+                        Note Taker is an application that can be used to write and save notes. Click on the image to view the live
+                        project or click here for the project repo.
+                    </p></a>
+                    <br />
+                    <br />
+                </div>
+
+                <div className="col-md-6"></div>
+
+                <div className="col-md-6"></div>
+
+
+                <div className="col-md-6">
+                    <a href="https://madeleinesc.github.io/work-day-scheduler/"><img src="./assets/workday.png" className="project-img img-fluid" data-aos="fade-up" /></a>
+                    <br />
+                    <br />
+                    <p className="lead" data-aos="fade-up">
+                        Work Scheduler - 2021
+                    </p>
+                    <a href="https://github.com/madeleinesc/work-day-scheduler" className="project-links" data-aos="fade-up"><p>
+                        A daily scheduler that changes color based on the time of day. Click on the image to view the live project or
+                        click here for the project repo.
+                    </p></a>
+                    <br />
+                    <br />
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Projects
