4cd22d201b4b503a5a3c3a5f92042abbdd9e0cd3
Madeleinesc <madeleine.s.cosway@gmail.com>
2022-01-31 20:05:11 +1030

    modified navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,28 +3,17 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light p-3 fixed-top">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#intro">Madeleine</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav ms-auto">
-                            <li className="nav-item">
-                                <a className="nav-link" href="#about">About</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#work">Work</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#contact">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+            <ul className="nav justify-content-center p-3">
+                <li className="nav-item">
+                    <a className="nav-link" href="#about"><p className="lead">About</p></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#work"><p className="lead">Work</p></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#contact"><p className="lead">Contact</p></a>
+                </li>
+            </ul>
         </div>
     )
 }
